e7617fb27ac151551b073369c898209e1574002c
Mister-None <mister_max@tutamail.com>
2026-03-15 18:30:32 +0200

    add some features

--- mh_bot.py
+++ mh_bot.py
@@ -2,7 +2,9 @@ import requests, json, os, sqlite3, re, subprocess, sys
 from moviepy import AudioFileClip
 from dotenv import load_dotenv
 from colorama import Fore, init
+from tqdm import tqdm
 import time
+
 load_dotenv()
 init(autoreset=True)
 
@@ -20,7 +22,7 @@ def sub(raw):
     return raw.lower().strip() 
 
 if len(sys.argv) < 2:
-    lst_functions = ['', 'add to base', 'clean metadata', 'play by arist', 'normalize', 'remove duplicactes']
+    lst_functions = ['', 'add to base', 'clean metadata', 'play by arist', 'normalize', 'remove duplicactes', 'update base']
     print(Fore.RED + 'Enter number of function!')
     [print(Fore.LIGHTYELLOW_EX + str(id), Fore.LIGHTGREEN_EX + '==>', Fore.LIGHTBLUE_EX +i) for id, i in enumerate(lst_functions) if i]
     sys.exit(1)
@@ -35,11 +37,12 @@ if entry == 0:
 
 elif entry == 1:
     counter = 0
-    for i in os.listdir():
-        if i.endswith('.mp3') or  i.endswith('.m4a') or i.endswith('.opus'):
+    for i in tqdm(os.listdir()):
+        if i.endswith('.opus'):
             track = AudioFileClip(i)
             duration = track.duration
-            if counter == 30:
+
+            if counter == 100:
                 print(Fore.LIGHTGREEN_EX + 'done')
                 con.commit()
                 cur.close()
@@ -52,8 +55,12 @@ elif entry == 1:
                 
                 try:
                     raw_data = subprocess.run(['songrec', 'recognize', 'sample.mp3', '--json'], capture_output=True, text=True)
-                    fine_data = json.loads(raw_data.stdout)
-                    time.sleep(5)
+                    try:
+                        fine_data = json.loads(raw_data.stdout)
+                    except json.decoder.JSONDecodeError as e:
+                        continue
+                    time.sleep(10)
+
                 except requests.exceptions.JSONDecodeError as e:
                     print(Fore.LIGHTRED_EX + str(e))
                     con.commit()
@@ -121,8 +128,23 @@ elif entry == 2:
         subprocess.run(['rm', MS_FOLDER+i])
 
 elif entry == 3:
-    artists = [i[0] for i in cur.execute('SELECT artist, count(artist) from items group by artist having count(artist) > 1 order by 1 DESC')]
-    print(*artists,  sep='\n')
+    if len(sys.argv) < 3:
+        sub_lst_functions = [['a', 'select by artist/band'], ['t', 'select by title'], ['g', 'select by genre'], ['y', 'select by year']]
+        print(Fore.RED + 'Enter number of function!')
+        [print(Fore.LIGHTYELLOW_EX + x[0], Fore.LIGHTGREEN_EX + '==>', Fore.LIGHTBLUE_EX + x[1]) for x in sub_lst_functions]
+        exit()
+    if sys.argv[2] == 'a':
+        selector = 'artist'
+    elif sys.argv[2] == 't':
+        selector = 'title'
+    elif sys.argv[2] == 'g':
+        selector = 'genre'
+    elif sys.argv[2] == 'y':
+        selector = 'date'
+    
+    data = [i[0] for i in cur.execute(f'SELECT distinct {selector} from items order by 1 DESC')]
+    print(*data,  sep='\n')
+
     entry = input('>>> ').split(' ')
     if len(entry) == 1:
         entry = f"('{entry[0]}')"
@@ -130,7 +152,8 @@ elif entry == 3:
         for id, i in enumerate(entry):
             entry[id] = f'{i}'
         entry = tuple(entry)
-    playlist = [i[0] for i in cur.execute(f"SELECT path from items where  artist in {entry} order by random()")]
+
+    playlist = [i[0] for i in cur.execute(f"SELECT path from items where {selector} in {entry} order by random()")]
     subprocess.run(['mpv'] + playlist)
 
 elif entry == 4:
@@ -152,6 +175,71 @@ elif entry == 5:
                 cur.execute(f"delete from items where id={j}")
                 subprocess.run(['rm', file])
 
+elif entry == 6:
+    entry1 = input('path >>> ').strip()
+    track = AudioFileClip(entry1)
+    duration = track.duration
+    sample = track.subclipped(1, duration//3).write_audiofile("sample.mp3", logger=None)
+    try:
+        raw_data = subprocess.run(['songrec', 'recognize', 'sample.mp3', '--json'], capture_output=True, text=True)
+        try:
+            fine_data = json.loads(raw_data.stdout)
+        except json.decoder.JSONDecodeError as e: 
+            print(e)
+            exit()
+            #continue
+        time.sleep(10)
+    except requests.exceptions.JSONDecodeError as e:
+        print(Fore.LIGHTRED_EX + str(e))
+        con.commit()
+        cur.close()
+        con.close()
+        exit()
+    try:
+        genre = fine_data['track']['genres']['primary']
+        genre = sub(genre)
+    except KeyError: genre ='unkown'
+
+    try: album = fine_data['track']['sections'][0]['metadata'][0]['text'].replace("'", '_')
+    except IndexError: album = 'unkown'
+    
+    try: label = fine_data['track']['sections'][0]['metadata'][1]['text'].replace("'", '_')
+    except IndexError: label = 'unkown'
+    
+    try: date = fine_data['track']['sections'][0]['metadata'][2]['text'].replace("'", '_')
+    except IndexError: date = 'unkown'
+
+    title = fine_data['track']['title']
+    title = sub(title)
+    artist = fine_data['track']['subtitle']
+    artist = sub(artist)
+    
+    music_data = [i[0] for i in cur.execute("select concat(artist, ' ', title) from items where artist !='unkown' and title !='unkown'")]
+
+    if str(artist + ' ' +  title) not in music_data:
+        
+        cur.execute(f"""UPDATE items SET 
+                            artist=?, 
+                            title=?, 
+                            album=?, 
+                            date=?, 
+                            label=?, 
+                            genre=?
+                        WHERE path=?""",
+                        [
+                            artist,
+                            title, 
+                            album, 
+                            date, 
+                            label, 
+                            genre,
+                            entry1
+                        ]
+                    )
+    else: 
+        print(Fore.LIGHTGREEN_EX + str(artist + ' ' +  title), 'in database!!!')
+    subprocess.run(['rm', 'sample.mp3'])
+   
 con.commit()
 cur.close()
 con.close()
